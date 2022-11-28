3010d5d6e36c2b70fac5fa20c494f41bff4256b0
Kat M <kmunson13@gmail.com>
2022-11-28 10:27:08 -0600

    Initial commit

---
+++ cv-project/src/App.js
@@ -0,0 +1,20 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div className="header">
+
+      </div>
+      <div className='body'>
+        <div className='left-side'>
+        </div>
+        <div className='right-side'>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
